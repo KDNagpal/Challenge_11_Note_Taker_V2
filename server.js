5241ba717920d68ebb01da536286cbdea7b4ad3c
KDNagpl <Karandeepnagpal2@gmail.com>
2023-03-11 22:46:32 -0500

    added a way to delete existing notes

--- server.js
+++ server.js
@@ -7,23 +7,23 @@ const filePath = path.join(__dirname, '/db/db.json');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //creates pathway
-app.get('/notes', (req,res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
 
 //route for getting all notes
-app.get('/api/notes', (req,res) => {
-const notes = getNotes()
-res.json(notes);
-console.log('Notes loaded!!')
+app.get('/api/notes', (req, res) => {
+    const notes = getNotes()
+    res.json(notes);
+    console.log('Notes loaded!!')
 });
 
 //post route for new notes
-app.post('/api/notes', (req,res) => {
+app.post('/api/notes', (req, res) => {
     const notes = getNotes()
     const newNote = {
         id: notes.length + 1,
@@ -34,20 +34,29 @@ app.post('/api/notes', (req,res) => {
     saveNotes(notes);
     res.json(newNote)
     console.log('Note created!!')
-    });
+});
 
-    //reads notes from .json file
-    function getNotes () {
-        const data = fs.readFileSync(path.join(__dirname,'db/db.json'), 'utf-8');
-        return JSON.parse(data);
+//deletes note
+app.delete('/api/notes/:id', (req, res) => {
+    const notes = getNotes();
+    const id = parseInt(req.params.id);
+    const filteredNotes = notes.filter(note => note.id !== id);
+    saveNotes(filteredNotes);
+    console.log('Note Deleted!!')
+  });
 
-    }
+//reads notes from .json file
+function getNotes() {
+    const data = fs.readFileSync(path.join(__dirname, 'db/db.json'), 'utf-8');
+    return JSON.parse(data);
 
-    //writes notes to .json file
-    function saveNotes(notes) {
-        const data =JSON.stringify(notes);
-        fs.writeFileSync(path.join(__dirname, 'db/db.json'), data);
-    }
+}
+
+//writes notes to .json file
+function saveNotes(notes) {
+    const data = JSON.stringify(notes);
+    fs.writeFileSync(path.join(__dirname, 'db/db.json'), data);
+}
 
 app.listen(PORT, () =>
-console.log(`Listening at http://localhost:${PORT}`));
+    console.log(`Server listening at http://localhost:${PORT}`));
